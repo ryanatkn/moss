e180d96bfd615e64c4e956d54cd1343df484faea
Ryan Atkinson <mail@ryanatkn.com>
2024-04-13 14:46:49 -0600

    wip

--- src/routes/library/themes/+page.svelte
+++ src/routes/library/themes/+page.svelte
@@ -8,12 +8,12 @@
 	import Color_Scheme_Input from '@ryanatkn/fuz/Color_Scheme_Input.svelte';
 	import Tome_Link from '@ryanatkn/fuz/Tome_Link.svelte';
 	import {get_theme, get_color_scheme} from '@ryanatkn/fuz/Themed.svelte';
-	import {default_themes} from '@ryanatkn/moss/themes.js';
 	import Tome_Subheading from '@ryanatkn/fuz/Tome_Subheading.svelte';
 	import Theme_Input from '@ryanatkn/fuz/Theme_Input.svelte';
 	import Mdn_Link from '@ryanatkn/fuz/Mdn_Link.svelte';
 	import Themed_Scope from '@ryanatkn/fuz/Themed_Scope.svelte';
 
+	import {default_themes} from '$lib/themes.js';
 	import {type Theme, save_theme} from '$lib/theme.js';
 	import Theme_Form from '$routes/Theme_Form.svelte';
 
