0938c90bb496057f8172e51cb5bb6eba73ea875b
Ryan Atkinson <mail@ryanatkn.com>
2024-04-13 23:13:10 -0600

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,24 +14,28 @@
 		<section class="box">
 			<h1 class="mt_xl4 mb_lg">moss</h1>
 			<div class="box mb_xl"><Moss_Logo size="120px" /></div>
-			<blockquote>CSS framework 🌿 magical organic stylesheets</blockquote>
-			<div class="width_sm panel p_md">
-				<p>
-					Moss is a CSS framework that's compatible with all websites and JS frameworks due to being
-					a single stylesheet. It's in early alpha and there will be many breaking changes.
-				</p>
-				<p>
-					Moss is being made to support <a href="https://www.ryanatkn.com/table"
-						>my other projects</a
-					>
-					that focus on end-users. It's grown slowly over 5 years to fit my needs, and my goal for 2024
-					is to fill out its API so it's more widely useful.
-				</p>
-				<p>
-					To learn more see <a href="{base}/library">the docs</a>
-					and <a href="https://github.com/ryanatkn/moss#readme">readme</a>. Feel free to take the
-					ideas and <a href="https://github.com/ryanatkn/moss">code</a> for your own purposes.
-				</p>
+			<blockquote class="shadow_inset_inverse_md">
+				CSS framework 🌿 magical organic stylesheets
+			</blockquote>
+			<div class="width_sm panel p_sm shadow_inset_inverse_md">
+				<div class="panel p_sm bg shadow_md">
+					<p>
+						Moss is a CSS framework that's compatible with all websites and JS frameworks due to
+						being a single stylesheet. It's in early alpha and there will be many breaking changes.
+					</p>
+					<p>
+						Moss is being made to support <a href="https://www.ryanatkn.com/table"
+							>my other projects</a
+						>
+						that focus on end-users. It's grown slowly over 5 years to fit my needs, and my goal for
+						2024 is to fill out its API so it's more widely useful.
+					</p>
+					<p>
+						To learn more see <a href="{base}/library">the docs</a>
+						and <a href="https://github.com/ryanatkn/moss#readme">readme</a>. Feel free to take the
+						ideas and <a href="https://github.com/ryanatkn/moss">code</a> for your own purposes.
+					</p>
+				</div>
 			</div>
 		</section>
 		<section>
