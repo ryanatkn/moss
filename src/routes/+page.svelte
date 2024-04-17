b6adb47884dfe50723aa91530562be6de5985ccc
Ryan Atkinson <mail@ryanatkn.com>
2024-04-17 16:48:25 -0600

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,8 @@
 			<blockquote class="shadow_inset_xs">magical organic stylesheets 🌿</blockquote>
 		</div>
 	</section>
-	<section class="width_sm panel pl_sm pt_md pr_xs shadow_inset_md">
-		<div class="panel p_sm bg shadow_md">
+	<section class="width_sm panel p_xs bg shadow_sm">
+		<div class="panel p_lg bg shadow_md">
 			<p>
 				Moss is a CSS framework that can be used with any website or JS framework. Moss has <a
 					href="https://github.com/ryanatkn/moss/blob/main/src/lib/style.css"
