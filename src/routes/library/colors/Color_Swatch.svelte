4dddf6cd1340532692a1bbe44a707286bea19c64
Ryan Atkinson <mail@ryanatkn.com>
2024-07-19 19:29:48 -0600

    fix colors in docs updating when color-scheme changes

--- src/routes/library/colors/Color_Swatch.svelte
+++ src/routes/library/colors/Color_Swatch.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import {hsl_to_hex_string, hsl_to_rgb, parse_hsl_string} from '@ryanatkn/belt/colors.js';
-
-	import Style_Variable_Button from '$routes/Style_Variable_Button.svelte';
+	import Color_Swatch_Item from '$routes/library/colors/Color_Swatch_Item.svelte';
 
 	interface Props {
 		color_name: string;
@@ -12,20 +10,8 @@
 </script>
 
 <ul class="unstyled">
-	{#each {length: 9} as _, i}
-		{@const num = i + 1}
-		{@const name = `color_${color_name}_${num}`}
-		{@const value = computed_styles?.getPropertyValue('--' + name)}
-		{@const hsl = value && parse_hsl_string(value)}
-		<li style:--bg_color="hsl(var(--{name}))">
-			<div class="color"></div>
-			<div class="text">
-				<Style_Variable_Button {name} />
-				<div class="hex">{hsl && hsl_to_hex_string(...hsl)}</div>
-				<div class="hsl">hsl({value})</div>
-				<div class="rgb">rgb({hsl && hsl_to_rgb(...hsl).join(' ')})</div>
-			</div>
-		</li>
+	{#each {length: 9} as _, index}
+		<Color_Swatch_Item {index} {color_name} {computed_styles} />
 	{/each}
 </ul>
 
@@ -33,47 +19,5 @@
 	ul {
 		width: 100%;
 		margin-bottom: var(--input_height_sm);
-	}
-	li {
-		display: flex;
-		align-items: stretch;
-		font-family: var(--font_mono);
-		min-height: var(--input_height_sm);
-	}
-	li:hover {
-		background-color: var(--bg);
-	}
-	.text {
-		display: flex;
-		align-items: center;
-		flex: 1;
-		padding-left: var(--space_sm);
-	}
-	.hex {
-		width: 73px;
-		font-size: var(--size_sm);
-		padding-left: var(--space_sm);
-	}
-	.hsl {
-		width: 149px;
-		font-size: var(--size_sm);
-		padding-left: var(--space_sm);
-	}
-	.rgb {
-		font-size: var(--size_sm);
-		padding-left: var(--space_sm);
-	}
-	.color {
-		width: 100px;
-		min-width: 50px;
-		background-color: var(--bg_color);
-		color: hsl(210, 55%, 62%);
-	}
-	@media (max-width: 630px) {
-		.text {
-			flex-direction: column;
-			align-items: flex-start;
-			padding: var(--space_sm);
-		}
 	}
 </style>
---
+++ src/routes/library/colors/Color_Swatch_Item.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import {hsl_to_hex_string, hsl_to_rgb, parse_hsl_string} from '@ryanatkn/belt/colors.js';
+	import {get_themer} from '@ryanatkn/fuz/theme.svelte.js';
+
+	import Style_Variable_Button from '$routes/Style_Variable_Button.svelte';
+
+	interface Props {
+		index: number;
+		color_name: string;
+		computed_styles: CSSStyleDeclaration | null;
+	}
+
+	const {index, color_name, computed_styles}: Props = $props();
+
+	const themer = get_themer();
+
+	const num = $derived(index + 1);
+	const name = $derived(`color_${color_name}_${num}`);
+	const value = $derived.by(() => {
+		// handle the user switching between light/dark mode
+		// TODO could refactor to a class for variables
+		themer.color_scheme;
+		return computed_styles?.getPropertyValue('--' + name);
+	});
+	const hsl = $derived(value && parse_hsl_string(value));
+</script>
+
+<li style:--bg_color="hsl(var(--{name}))">
+	<div class="color"></div>
+	<div class="text">
+		<Style_Variable_Button {name} />
+		<div class="hex">{hsl && hsl_to_hex_string(...hsl)}</div>
+		<div class="hsl">hsl({value})</div>
+		<div class="rgb">rgb({hsl && hsl_to_rgb(...hsl).join(' ')})</div>
+	</div>
+</li>
+
+<style>
+	li {
+		display: flex;
+		align-items: stretch;
+		font-family: var(--font_mono);
+		min-height: var(--input_height_sm);
+	}
+	li:hover {
+		background-color: var(--bg);
+	}
+	.text {
+		display: flex;
+		align-items: center;
+		flex: 1;
+		padding-left: var(--space_sm);
+	}
+	.hex {
+		width: 73px;
+		font-size: var(--size_sm);
+		padding-left: var(--space_sm);
+	}
+	.hsl {
+		width: 149px;
+		font-size: var(--size_sm);
+		padding-left: var(--space_sm);
+	}
+	.rgb {
+		font-size: var(--size_sm);
+		padding-left: var(--space_sm);
+	}
+	.color {
+		width: 100px;
+		min-width: 50px;
+		background-color: var(--bg_color);
+		color: hsl(210, 55%, 62%);
+	}
+	@media (max-width: 630px) {
+		.text {
+			flex-direction: column;
+			align-items: flex-start;
+			padding: var(--space_sm);
+		}
+	}
+</style>
