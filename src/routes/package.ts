e6bff63e2ab1120fb2895643a6877fd203e2920b
Ryan Atkinson <mail@ryanatkn.com>
2024-09-12 16:23:56 -0600

    wip

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -48,7 +48,7 @@ export const package_json = {
 		'prism-svelte': '^0.5.0',
 		prismjs: '^1.29.0',
 		svelte: '^5.0.0-next.245',
-		'svelte-check': '^4.0.1',
+		'svelte-check': '^4.0.2',
 		tslib: '^2.7.0',
 		typescript: '^5.6.2',
 		'typescript-eslint': '^8.5.0',
