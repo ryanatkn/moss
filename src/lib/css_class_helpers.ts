870b884ea976818a779cac5208ccbf30e69763ef
Ryan Atkinson <mail@ryanatkn.com>
2024-10-03 03:37:45 -0600

    wip

--- src/lib/css_class_helpers.ts
+++ src/lib/css_class_helpers.ts
@@ -1,17 +1,20 @@
-// class="..."
-const CLASS_ATTR_MATCHER = /class=["']([^"']+)["']/g;
+// class="a b"
+const CLASS_ATTR_MATCHER = /class=["'`]([^"'`]+)["'`]/g;
 
-// class:...
-const CLASS_DIRECTIVE_MATCHER = /class:([^=]+)=["']([^"']+)["']/g;
+// class:a
+const CLASS_DIRECTIVE_MATCHER = /class:([^=]+)=["'`]([^"'`]+)["'`]/g;
 
-// classes="..."
-const CLASSES_PROP_MATCHER = /classes=["']([^"']+)["']/g;
+// classes="a b"
+const CLASSES_PROP_MATCHER = /classes=["'`]([^"'`]+)["'`]/g;
 
-// *classes = ['...']
-const CLASSES_ARRAY_LITERAL_MATCHER = /\*classes\s*=\s*\[([^\]]+)\]/g;
+// *classes = ['a', 'b']
+const CLASSES_ARRAY_LITERAL_MATCHER = /classes\s*=\s*\[([^\]]+)\]/gi;
 
-// *classes = '...'
-const CLASSES_STRING_LITERAL_MATCHER = /\*classes\s*=\s*'([^']+)'/g;
+// *classes = 'a b'
+// TODO BLOCK `'"
+const CLASSES_STRING_LITERAL_MATCHER = /classes\s*=\s*["'`]([^["'`]]+)["'`]/gi;
+
+// TODO BLOCK tests with all forms, including multiline
 
 /**
  * Returns an array of CSS classes from a string of HTML content.
@@ -22,7 +25,7 @@ export const collect_css_classes = (contents: string): string[] => {
 	// TODO BLOCK some false positives
 	const classes: string[] = [];
 	const add_classes = (match: string, class_list: string) => {
-		classes.push(...class_list.split(/\s+/));
+		classes.push(...class_list.split(/\s+/).filter(Boolean));
 		return match;
 	};
 	contents.replace(CLASS_ATTR_MATCHER, add_classes);
