b27e38c726e7b15c44f776e3570316d6facb8c44
Ryan Atkinson <mail@ryanatkn.com>
2024-10-03 03:19:11 -0600

    wip

--- gro.config.ts
+++ gro.config.ts
@@ -2,6 +2,8 @@ import type {Create_Gro_Config} from '@ryanatkn/gro';
 
 import {gro_plugin_moss_css} from '$lib/gro_plugin_moss_css.js';
 
+// TODO delete this after being added to Gro's base config (try/catch around a blind import)
+
 export const config: Create_Gro_Config = (cfg) => {
 	const get_base_plugins = cfg.plugins;
 	cfg.plugins = async (ctx) => [gro_plugin_moss_css()].concat(await get_base_plugins(ctx));
