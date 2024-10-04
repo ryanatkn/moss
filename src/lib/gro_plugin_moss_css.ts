f0cf82341d5a5cdbdfa27cbaef829f3c70b47025
Ryan Atkinson <mail@ryanatkn.com>
2024-10-03 21:03:53 -0600

    wip

--- src/lib/gro_plugin_moss_css.ts
+++ src/lib/gro_plugin_moss_css.ts
@@ -42,8 +42,8 @@ export interface Options {
 
 export const gro_plugin_moss_css = ({
 	include_classes = null,
-	outfile = 'src/routes/moss.css', // TODO BLOCK what about multiple files using file filters to check where to collect them?
-	filter_file = null, //(p) => !p.includes('.test.'),
+	outfile = 'src/routes/moss.css', // TODO maybe support multiple files using file filters to check where to collect them?
+	filter_file = (p) => !p.includes('.test.') && !p.includes('/test/'),
 	flush_debounce_delay = FLUSH_DEBOUNCE_DELAY,
 }: Options = EMPTY_OBJECT): Plugin => {
 	const css_classes = new Css_Classes(
