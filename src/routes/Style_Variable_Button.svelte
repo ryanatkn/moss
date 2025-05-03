6ff23beb0bce7d8710a595b9a0ccbdd31a611e7b
Ryan Atkinson <mail@ryanatkn.com>
2025-05-03 16:26:51 -0600

    wip

--- src/routes/Style_Variable_Button.svelte
+++ src/routes/Style_Variable_Button.svelte
@@ -36,7 +36,8 @@
 	class:inline
 	class:plain
 	onclick={() => (selected_variable.value = variable)}
-	>{#if children}{@render children()}{:else}<span class="font_family_mono">{name}</span>{/if}</button
+	>{#if children}{@render children()}{:else}<span class="font_family_mono">{name}</span
+		>{/if}</button
 >
 
 <style>
