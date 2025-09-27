b5a3edbebbc83e7563414129b1d7ada1a0775cb2
Ryan Atkinson <mail@ryanatkn.com>
2025-09-26 22:15:35 -0400

    wip

--- src/fixtures/css_classes_fixture.gen.json.ts
+++ src/fixtures/css_classes_fixture.gen.json.ts
@@ -3,5 +3,5 @@ import type {Gen} from '@ryanatkn/gro';
 import {css_classes_by_name} from '$lib/css_classes.js';
 
 export const gen: Gen = () => {
-	return JSON.stringify(css_classes_by_name, null, '\t');
+	return JSON.stringify(css_classes_by_name);
 };
